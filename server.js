9161b210ce77819fdcf942e5a5b97a69695ec5d1
kunal <kunal.patel@elevenrobots.com>
2018-04-12 13:43:11 +0100

    updated ip for openshift

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var sqlite = require('sqlite-sync');
 const app = express()
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 
 function GetData1()
